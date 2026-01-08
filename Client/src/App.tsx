75d49e45d76d1f2b46cf96421727a4ebd5d02bd3
chandan123-sik <sikarwarnihal191@gmail.com>
2026-01-08 22:29:14 +0530

    Initial Commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./globals.css";
+import LenisScroll from "./components/LenisScroll";
+import YtPreview from "./pages/YtPreview";
+import MyGeneration from "./pages/MyGeneration";
+import Generate from "./pages/Generate";
+import Login from "./components/Login"
+export default function App() {
+    return (
+        <>
+            <LenisScroll />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/generate" element={<Generate />} />
+                <Route path="/generate/:id" element={<Generate />} />
+                <Route path="/my-generation" element={<MyGeneration />} />
+                <Route path="/preview" element={<YtPreview />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </>
+    );
+}
---
+++ Client/src/data/features.tsx
@@ -0,0 +1,19 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Our AI Analyzes Video Content To Suggest The Most Clickable Concept.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye-Catching Designs",
+        description: "Generate Vibrant, High-Contrast Thumbnails That Stand Out in the Feed.",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Get Fully Layered Design You Can Tweak To Perfection If Needed.",
+    }
+];
