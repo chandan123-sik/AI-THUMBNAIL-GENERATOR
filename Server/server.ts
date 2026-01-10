b6a5d0ca3d1541a5c84c1ec7263b5d3f95e28254
chandan123-sik <sikarwarnihal191@gmail.com>
2026-01-10 16:23:49 +0530

    Making Server Using Express

---
+++ Server/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from "cors";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
